0dc0266ddffe9a678dc782b733bf8d141ee48ef2
Vedant Pandey <vedantpandey46@gmail.com>
2019-12-02 00:23:43 +0530

    SSL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express'),
-      path    = require('path'),
       app     = express()
 
 app.use(express.static(__dirname + '/dist'))
+
+const forceSSL = () => {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://',req.get('Host'), req.url].join('')
+      )
+    }
+    next()
+  }
+}
+
+app.use(forceSSL())
 
 app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/dist/prod/index.html')
